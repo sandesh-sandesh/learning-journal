70535c8f940f19b4697c42c837dfd2706c01f502
Sandesh Bhaigade <125783419+sandesh-sandesh@users.noreply.github.com>
2026-05-13 22:35:16 +0530

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,26 @@
+one=float(input("first number "))
+two=float(input("second number "))
+print("choose operaion")
+print("1 = add")
+print("2 = sub")
+print("3 = multiply")
+print("4 = divide")
+
+choice=input("enter operations with comma ")
+selected=choice.split(",")
+
+for choice in selected:
+if choice=="1":
+print("result ", one+two)
+elif choice=="2":
+print("result ", one-two)
+elif choice=="3":
+print("result ", one*two)
+elif choice=="4":
+print("result ", one/two)
+else:
+print("invalid input")
+
+
+
+
