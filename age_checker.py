df51adeae6082b257212d5f79001d973cda32d0a
Sandesh Bhaigade <125783419+sandesh-sandesh@users.noreply.github.com.>
2026-05-16 18:54:59 +0530

    age checking

---
+++ age_checker.py
@@ -0,0 +1,8 @@
+age = int(input ("enter your age"))
+
+if age > 21:
+    print(" you are eligible for the course")
+elif age < 35:
+    print(" you are eligible for the course")
+else :
+    print(" you are not eligible for the course")
