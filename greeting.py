6a33a7150b7bc6f54ed4c61cb193f040391002ea
Sandesh Bhaigade <125783419+sandesh-sandesh@users.noreply.github.com>
2026-07-30 22:32:56 +0530

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -1,4 +1,4 @@
-name = input("What is your name? ") #this is a code greet and then it also tells the length of the string 
+name = input("What is your name? ") #this is a code greet and then it also tells the length of the string. 
 
 print("Hellooooo", name)
 print(" Welcome to Day 5 of GitHub learning ")
