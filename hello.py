2d802b5f5a991b968bf4e6dba69ce7a8ee7ef418
Sandesh Bhaigade <125783419+sandesh-sandesh@users.noreply.github.com>
2026-08-19 09:37:14 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,2 @@
-print("hellllo people I am learning to use git adn github") #this is a hello printing code.
+print("helllllo people I am learning to use git adn github") #this is a hello printing code.
 print(" welllll , I have a python exam ")
