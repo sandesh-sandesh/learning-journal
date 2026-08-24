7897fa179dec493e4d43cce728fdb4e866499417
Sandesh Bhaigade <125783419+sandesh-sandesh@users.noreply.github.com>
2026-08-25 01:43:34 +0530

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -1,6 +1,6 @@
 name = input("What is your name? ") #this is a code greeeet and then it also tells the length of the string. 
 
-print("Helloooooo", name)
+print("Hellooooooo", name)
 print(" Welcome to Day 5 of GitHub learning ")
 print(len(name))
 
